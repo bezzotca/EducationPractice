bde298f112ecbd638927293909fd033e666602f2
Andrey Kuznetsov <andreisk2005@gmail.com>
2025-03-05 09:31:23 +0300

    First commit

---
+++ EducationPractice/EducationPractice/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EducationPractice.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ EducationPractice/EducationPractice/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EducationPractice.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
